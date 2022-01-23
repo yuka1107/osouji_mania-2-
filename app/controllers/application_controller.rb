a159130413fee163e800ee6ee657ad8ac7da7bdc
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2022-01-23 09:05:20 +0000

    devise:views作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる場合、その前にconfigure_permitted_parametersが実行される。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :introduction, :is_active, :admin_name])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  
+
+  devise_for :admins
+
+  devise_for :users
+
   scope module: :public do
     root to: 'homes#top'
   end
-  
+
   namespace :admin do
     root to: '/admin/homes#top'
   end
-  
+
 end
